9eb07f514e1d1861a12a5d08afbddb3cd01026d6
Felipe Louzeiro <fl.louzeiro@gmail.com>
2023-06-07 19:22:10 -0300

    deploy: corrigindo variaveis de ambiente

--- src/main/java/com/spring/blog/utils/DummyData.java
+++ src/main/java/com/spring/blog/utils/DummyData.java
@@ -29,13 +29,13 @@ public class DummyData {
         List<Post> postList = new ArrayList<>();
 
         Post post1 = new Post();
-        post1.setAuthor("Bruno Alexandre");
+        post1.setAuthor("Felipe Louzeiro Alexandre");
         post1.setDate(LocalDate.now());
         post1.setTitle("Docker");
         post1.setContent("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 
         Post post2 = new Post();
-        post2.setAuthor("Michelli Brito");
+        post2.setAuthor("Felipe Louzeiro");
         post2.setDate(LocalDate.now());
         post2.setTitle("API REST");
         post2.setContent("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
